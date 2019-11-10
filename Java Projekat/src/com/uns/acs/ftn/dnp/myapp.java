c821fb228c0c4e0a2606e728f05f88a9ab8a7a9b
Nikola Kolovic <nikoladskv@hotmail.rs>
2019-11-10 01:05:49 +0100

    prvi commit

--- Java Projekat/src/com/uns/acs/ftn/dnp/myapp.java
+++ Java Projekat/src/com/uns/acs/ftn/dnp/myapp.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 public class myapp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		MainFrame mf= new MainFrame();
 		mf.setVisible(true);
 	}
