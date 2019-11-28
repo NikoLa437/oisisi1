35f980a3a51c12f764452ad1c2e36e2994f5b1cf
Dusan Petrovic <dule_srb@hotmail.rs>
2019-11-28 20:06:07 +0100

    #apstraktna_akcija_za_dodavanje_entiteta(predmet)

---
+++ Java Projekat/src/rs/ac/uns/ftn/projekat/actions/CreateEntityAction.java
@@ -0,0 +1,34 @@
+package rs.ac.uns.ftn.projekat.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import javax.swing.KeyStroke;
+
+import rs.ac.uns.ftn.projekat.dialogs.DodajPredmet;
+import rs.ac.uns.ftn.projekat.view.JTabbedPaneTabele;
+
+public class CreateEntityAction extends AbstractAction{
+
+
+	private static final long serialVersionUID = 1218029309718581204L;
+
+	public CreateEntityAction() {
+
+		putValue(MNEMONIC_KEY, KeyEvent.VK_A);
+		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_A,KeyEvent.CTRL_MASK));
+	
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if(JTabbedPaneTabele.selektovan_tab == 2) {
+			DodajPredmet dp = new DodajPredmet(new JFrame());
+		}
+		
+	}
+	
+	
+
+}
