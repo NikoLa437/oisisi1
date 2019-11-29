425075884a052c571e4b0fc7ffcbc2089d6a0e77
Nikola Kolovic <nikoladskv@hotmail.rs>
2019-11-29 01:21:23 +0100

    #abstract model studenta

---
+++ Java Projekat/src/rs/ac/uns/ftn/projekat/view/AbstractTableModelStudent.java
@@ -0,0 +1,36 @@
+package rs.ac.uns.ftn.projekat.view;
+
+import javax.swing.table.AbstractTableModel;
+
+import rs.ac.uns.ftn.projekat.data.BazaStudent;
+
+public class AbstractTableModelStudent extends AbstractTableModel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return BazaStudent.getInstance().getStudenti().size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return  BazaStudent.getInstance().getColumnCount();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return BazaStudent.getInstance().getValueAt(rowIndex, columnIndex);
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return BazaStudent.getInstance().getColumnName(column);
+	}
+}
