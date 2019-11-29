e23a42ce610a318fb4859d105cbc7a1f1024f437
Nikola Kolovic <nikoladskv@hotmail.rs>
2019-11-29 01:21:04 +0100

    #student_table

---
+++ Java Projekat/src/rs/ac/uns/ftn/projekat/view/StudentJTable.java
@@ -0,0 +1,64 @@
+package rs.ac.uns.ftn.projekat.view;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.util.ArrayList;
+
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.RowSorter;
+import javax.swing.SortOrder;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
+public class StudentJTable extends JTable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	static TableModel model;
+	
+	public StudentJTable() {
+		this.setRowSelectionAllowed(true);
+		this.setColumnSelectionAllowed(true);
+		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); // ?
+		this.setModel(new AbstractTableModelStudent()); //
+		this.getTableHeader().setReorderingAllowed(false);
+		this.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		
+		TableRowSorter<TableModel> sorter = new TableRowSorter<>(this.getModel());
+		this.setRowSorter(sorter);
+		ArrayList<RowSorter.SortKey> sortKeys = new ArrayList<RowSorter.SortKey>();
+		
+	    model = this.getModel();
+		
+		int columnIndexToSort = 1;
+		sortKeys.add(new RowSorter.SortKey(columnIndexToSort, SortOrder.ASCENDING));
+		
+		sorter.setSortKeys(sortKeys);
+		sorter.sort();
+		
+		//sorter.setSortable(6, false);
+	}
+	
+	public static void osvezi() {
+		((AbstractTableModel) model).fireTableDataChanged();
+	}
+	
+	@Override
+	public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+		Component c = super.prepareRenderer(renderer, row, column);
+		if (isRowSelected(row)) {
+			c.setBackground(Color.LIGHT_GRAY);
+		} else {
+			c.setBackground(Color.WHITE);
+		}
+		return c;
+	}
+}
