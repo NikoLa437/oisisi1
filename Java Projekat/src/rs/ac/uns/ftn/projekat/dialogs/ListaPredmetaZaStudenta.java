f20d8842aeb0e870aee11d46f4dcc5e52de9dc4b
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-01-12 14:15:29 +0100

    #ListaPredmetaZaStudenta izmene, sredjen dijalog

--- Java Projekat/src/rs/ac/uns/ftn/projekat/dialogs/ListaPredmetaZaStudenta.java
+++ Java Projekat/src/rs/ac/uns/ftn/projekat/dialogs/ListaPredmetaZaStudenta.java
@@ -1,7 +1,6 @@
 package rs.ac.uns.ftn.projekat.dialogs;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -11,6 +10,7 @@ import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JList;
@@ -20,6 +20,7 @@ import javax.swing.ListSelectionModel;
 
 import rs.ac.uns.ftn.projekat.classes.Predmet;
 import rs.ac.uns.ftn.projekat.data.BazaStudent;
+import rs.ac.uns.ftn.projekat.view.MainFrame;
 
 
 public class ListaPredmetaZaStudenta extends JDialog{
@@ -32,10 +33,9 @@ public class ListaPredmetaZaStudenta extends JDialog{
 	public ListaPredmetaZaStudenta(JFrame parent,int selrow) {
 		super(parent,"Lista studenata",true);
 		
-		//String selektovaniIndeks="";
-		this.setSize(300,200);
+		this.setSize(MainFrame.sirina*2/7,MainFrame.visina*3/7);
 		this.setLayout(new BorderLayout());
-
+		//paneli p1 i p2 da naprave razmak
 		JPanel p1= new JPanel();
 		p1.setPreferredSize(new Dimension(30,20));
 		JPanel p2= new JPanel();
@@ -43,23 +43,23 @@ public class ListaPredmetaZaStudenta extends JDialog{
 		JPanel panelC = new JPanel(new FlowLayout(FlowLayout.CENTER));  // panel za unos 
 		JPanel panelS = new JPanel(new FlowLayout(FlowLayout.RIGHT));  // panel za button-e
 		
-		Button bOdustani=new Button("Odustani");
+		JButton bOdustani=new JButton("Odustani");
 		
 		///////////////////////////////////////////////////////////////
 		DefaultListModel<String> listModel = new DefaultListModel<>();
 		
-		ArrayList<Predmet> listaPredmeta= BazaStudent.getInstance().getRow(selrow).getPredmeti();
+		ArrayList<Predmet> listaPredmeta = BazaStudent.getInstance().getRow(selrow).getPredmeti();
 		
 		for(Predmet p : listaPredmeta) {
 			listModel.addElement(p.getNaziv());
 		}
 
 		JList<String> lista = new JList<>(listModel);
-		lista.setFixedCellWidth(100);
+		lista.setFixedCellWidth(200);
 		lista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		lista.setBorder(BorderFactory.createLineBorder(Color.black));
 		
-		lista.setVisibleRowCount(5);
+		lista.setVisibleRowCount(8);
 		
 		bOdustani.addActionListener(new ActionListener(){
 			@Override
@@ -79,7 +79,7 @@ public class ListaPredmetaZaStudenta extends JDialog{
 		this.add(panelC,BorderLayout.CENTER);
 		
 		this.add(panelS,BorderLayout.SOUTH);
-		this.setTitle("Lista studenata");  
+		this.setTitle("Lista predmeta");  
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
